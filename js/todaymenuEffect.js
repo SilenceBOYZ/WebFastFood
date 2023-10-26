e8d194038841c080ed82403bfe216488f3e2e5f0
SilenceBOYZ <nguyenthanhtri230799@gmail.com>
2023-10-26 11:17:42 +0700

    Responsive Layout

--- js/todaymenuEffect.js
+++ js/todaymenuEffect.js
@@ -13,3 +13,42 @@ menus.forEach(menu => {
 const icon = document.querySelectorAll(".shop-menu__statusIcon");
 
 console.log(icon);
+
+
+// Làm phần tăng giảm item
+
+let inputValue = document.querySelectorAll(".item-Details__input");
+let btnItem = document.querySelectorAll(".item-Details__button");
+let itemPrice = document.querySelector(".item-Details__price");
+
+
+inputValue.forEach(input => {
+  btnItem.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (btn.id === "left") {
+        let ketqua = parseInt(input.value);
+        if (ketqua <= 1) {
+          input.value = 1;
+        } else {
+          ketqua -= 1;
+          input.value = ketqua.toString();
+        }
+
+      } else {
+        let ketqua = parseInt(input.value);
+        ketqua += 1;
+        input.value = ketqua.toString();
+      }
+    });
+  })
+})
+
+
+const toggleBtn = document.querySelector(".toggle");
+const navBarMobile = document.querySelector(".navbar-mobile");
+
+toggleBtn.onclick = () => {
+  toggleBtn.classList.toggle("toggle__active");
+  navBarMobile.classList.toggle("active__mobile");
+}
