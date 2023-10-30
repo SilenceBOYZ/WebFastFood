5cf11255baeb33724458344bea7915d3a481cc8d
SilenceBOYZ <nguyenthanhtri230799@gmail.com>
2023-10-30 22:39:42 +0700

    Update Responsive Layout

--- js/todaymenuEffect.js
+++ js/todaymenuEffect.js
@@ -52,3 +52,43 @@ toggleBtn.onclick = () => {
   toggleBtn.classList.toggle("toggle__active");
   navBarMobile.classList.toggle("active__mobile");
 }
+
+
+const loginAction = document.querySelectorAll(".login-action");
+const loginSite = document.querySelector(".login-site");
+let loginPage = loginSite.querySelectorAll(".login-site__wrapper");
+let closeBtn = document.querySelectorAll(".login-site__button");
+let btnChangeSite = document.querySelectorAll(".btn-changeSite");
+
+loginAction.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (btn.id === "login") {
+      loginSite.style.opacity = "1";
+      loginSite.style.visibility = "visible";
+      loginPage[0].classList.add("activeSite");
+      loginPage[1].classList.remove("activeSite");
+      // loginSite[0].style.transform = "translate-x(100%)";
+    } else {
+      loginSite.style.opacity = "1";
+      loginSite.style.visibility = "visible";
+      loginPage[0].classList.remove("activeSite");
+      loginPage[1].classList.add("activeSite");
+    }
+  });
+});
+
+closeBtn.forEach(btn => {
+  btn.onclick = (e) => {
+    loginSite.style.opacity = "0";
+    loginSite.style.visibility = "hidden";
+  }
+});
+
+btnChangeSite.forEach(btnChangeSite => {
+  btnChangeSite.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginPage[0].classList.toggle("activeSite");
+    loginPage[1].classList.toggle("activeSite");
+  })
+})
