4790b9688f864132ed8cc99a6addbc56b3d232f2
SilenceBOYZ <nguyenthanhtri230799@gmail.com>
2023-11-17 14:21:08 +0700

    update slide catergories moving

--- js/catergoriesAuto.js
+++ js/catergoriesAuto.js
@@ -1,22 +1,44 @@
 
 const items = document.querySelectorAll(".order-menu__itemAction");
 let containSlide = document.querySelector(".order-menu__titleContain");
+let btns = document.querySelectorAll(".order-menu__btnMoveSlide");
 let previousXPosition = 0;
 let positionToMove = -145;
+
 let CountItem = 0;
-function updateI() {
-  if (CountItem === items.length) {
+
+
+function slideMoving() {
+  if (CountItem > items.length - 5) {
     previousXPosition = 0;
     CountItem = 0;
+    containSlide.style.transition = `none`;
     containSlide.style.transform = `translate3d(${previousXPosition}px, 0px, 0px)`;
     return;
   }
+  containSlide.style.transition = "2s all";
   CountItem++;
   containSlide.style.transform = `translate3d(${previousXPosition += positionToMove}px, 0px, 0px)`
+  positionToMove = -145;
 }
 
-let Count = () => {
-  setInterval(updateI, 5000);
+
+btns.forEach((btn) => {
+  btn.onclick = () => {
+    if (btn.id === "movingLeft") {
+      if(previousXPosition === 0) return;
+      CountItem--;
+      positionToMove = 145;
+      slideMoving();
+    } else if (btn.id === "movingRight") {
+      slideMoving();
+    }
+  }
+})
+
+
+let timeToSlide = () => {
+  setInterval(slideMoving, 5000);
 }
 
-Count();
+timeToSlide();
