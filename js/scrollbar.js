6b925906b2a9969f95bec99f42aa9014ac2d02bc
SilenceBOYZ <nguyenthanhtri230799@gmail.com>
2023-12-01 13:21:30 +0700

    fix something

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,32 +1,35 @@
 const bannerHome = document.querySelector('.banner-homepage');
 const bannerShop = document.querySelector('.shopStyle-banner');
-const headerScroll = [
-  bannerHome,
-  bannerShop
-];
 
-const obs = new IntersectionObserver(function(entries){
-  const ent = entries[0];
-  console.log(ent);
-  if(ent.isIntersecting === false){
-    document.body.classList.add('sticky');
+  const headerScroll = [
+    bannerHome,
+    bannerShop
+  ];
+  
+  const obs = new IntersectionObserver(function(entries){
+    const ent = entries[0];
+    console.log(ent);
+    if(ent.isIntersecting === false){
+      document.body.classList.add('sticky');
+    }
+  
+    if(ent.isIntersecting){
+      document.body.classList.remove('sticky');
+    }
+  }, {
+    // In the viewport
+    // root 
+    root: null,
+    threshold: 0,
+    rootMargin: '0px'
+  });
+  
+  for(let i = 0; i < headerScroll.length; i++){
+    if(headerScroll[i] === null){
+      continue;
+    }
+    obs.observe(headerScroll[i]);
   }
 
-  if(ent.isIntersecting){
-    document.body.classList.remove('sticky');
-  }
-}, {
-  // In the viewport
-  // root 
-  root: null,
-  threshold: 0,
-  rootMargin: '0px'
-});
 
-for(let i = 0; i < headerScroll.length; i++){
-  if(headerScroll[i] === null){
-    continue;
-  }
-  obs.observe(headerScroll[i]);
-}
 // 
